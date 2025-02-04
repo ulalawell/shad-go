cd519511e11c40288821d2446b347ef43c750ccf
ulalawell <roman2000.usik@gmail.com>
2025-02-04 21:38:29 +0300

    feat: Add tasks tour, wordcount

--- wordcount/main.go
+++ wordcount/main.go
@@ -1,7 +1,34 @@
-//go:build !solution
-
 package main
 
-func main() {
+import (
+	"fmt"
+	"os"
+	"strings"
+)
 
+func check(e error) {
+	if e != nil {
+		panic(e)
+	}
+}
+
+func main() {
+	files := os.Args[1:]
+	countByWord := map[string]int{}
+
+	for _, file := range files {
+		data, error := os.ReadFile(file)
+		check(error)
+		lines := strings.Split(string(data), "\n")
+
+		for _, line := range lines {
+			countByWord[line]++
+		}
+	}
+
+	for word, count := range countByWord {
+		if count > 1 {
+			fmt.Printf("%d\t%s\n", count, word)
+		}
+	}
 }
