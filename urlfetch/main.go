fc19ddae792206e0fe1341ea9585614499f8a4d1
ulalawell <roman2000.usik@gmail.com>
2025-02-06 22:57:18 +0300

    feat: Add task urlfetch

--- urlfetch/main.go
+++ urlfetch/main.go
@@ -2,6 +2,31 @@
 
 package main
 
-func main() {
+import (
+	"fmt"
+	"io"
+	"net/http"
+	"os"
+)
 
+func panic(err error) {
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(-1)
+	}
+}
+
+func main() {
+	urls := os.Args[1:]
+	for _, url := range urls {
+		resp, err := http.Get(url)
+		panic(err)
+
+		defer resp.Body.Close()
+
+		body, err := io.ReadAll(resp.Body)
+		panic(err)
+
+		fmt.Println(string(body))
+	}
 }
