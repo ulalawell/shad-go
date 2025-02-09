a8c495b344b3499f7bbbdbaec00878332e513735
ulalawell <roman2000.usik@gmail.com>
2025-02-09 22:34:25 +0300

    feat: Add task hotels

--- hotelbusiness/hotels.go
+++ hotelbusiness/hotels.go
@@ -2,6 +2,8 @@
 
 package hotelbusiness
 
+import "sort"
+
 type Guest struct {
 	CheckInDate  int
 	CheckOutDate int
@@ -13,5 +15,49 @@ type Load struct {
 }
 
 func ComputeLoad(guests []Guest) []Load {
-	return []Load{}
+	starts := make([]int, len(guests))
+	ends := make([]int, len(guests))
+	loads := []Load{}
+
+	for index, guest := range guests {
+		starts[index] = guest.CheckInDate
+		ends[index] = guest.CheckOutDate
+	}
+
+	sort.Ints(starts)
+	sort.Ints(ends)
+	currentLoad := 0
+
+	startPtr, endPtr := 0, 0
+	for startPtr < len(starts) || endPtr < len(ends) {
+		if startPtr != len(starts) && ends[endPtr] > starts[startPtr] {
+			currentLoad++
+			for startPtr < len(starts)-1 && starts[startPtr] == starts[startPtr+1] {
+				startPtr++
+				currentLoad++
+			}
+			loads = append(loads, Load{StartDate: starts[startPtr], GuestCount: currentLoad})
+
+			startPtr++
+			continue
+		}
+
+		if (startPtr != len(starts) && ends[endPtr] < starts[startPtr]) || startPtr == len(starts) {
+			currentLoad--
+			for endPtr < len(ends)-1 && ends[endPtr] == ends[endPtr+1] {
+				endPtr++
+				currentLoad--
+			}
+
+			loads = append(loads, Load{StartDate: ends[endPtr], GuestCount: currentLoad})
+			endPtr++
+			continue
+		}
+
+		startPtr++
+		endPtr++
+
+	}
+
+	return loads
 }
