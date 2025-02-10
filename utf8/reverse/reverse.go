623de23f0c3ab74f49898f3eaf0a1a33a3a475ec
ulalawell <roman2000.usik@gmail.com>
2025-02-10 22:39:17 +0300

    feat: Add task reverse utf8

--- utf8/reverse/reverse.go
+++ utf8/reverse/reverse.go
@@ -2,6 +2,21 @@
 
 package reverse
 
+import (
+	"strings"
+	"unicode/utf8"
+)
+
 func Reverse(input string) string {
-	return ""
+	var b strings.Builder
+	b.Grow(len(input))
+
+	for len(input) > 0 {
+		symbol, size := utf8.DecodeLastRuneInString(input)
+		input = input[:len(input)-size]
+		b.WriteRune(symbol)
+
+	}
+
+	return b.String()
 }
