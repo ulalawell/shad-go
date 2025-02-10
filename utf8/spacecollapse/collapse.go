a45d0585739e539f991cccc1eaa35e05b44cabc7
ulalawell <roman2000.usik@gmail.com>
2025-02-10 23:13:07 +0300

    feat: Add task collapse utf8

--- utf8/spacecollapse/collapse.go
+++ utf8/spacecollapse/collapse.go
@@ -2,6 +2,43 @@
 
 package spacecollapse
 
+import (
+	"strings"
+	"unicode/utf8"
+)
+
 func CollapseSpaces(input string) string {
-	return ""
+	var sb strings.Builder
+	symbolsToChange := map[rune]struct{}{
+		'\r': {},
+		'\n': {},
+		'\t': {},
+		' ':  {},
+	}
+
+	for i := 0; i < len(input); {
+		needSpace := false
+		for j := i; j < len(input); {
+			symbol, size := utf8.DecodeRuneInString(input[i:])
+			if _, ok := symbolsToChange[symbol]; !ok {
+				break
+			}
+			needSpace = true
+			j += size
+			i += size
+		}
+
+		if needSpace {
+			sb.WriteRune(' ')
+		}
+
+		if i < len(input) {
+			symbol, size := utf8.DecodeRuneInString(input[i:])
+
+			sb.WriteRune(symbol)
+			i += size
+		}
+
+	}
+	return sb.String()
 }
