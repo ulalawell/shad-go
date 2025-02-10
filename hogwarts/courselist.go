07b7375cdb38746c48b55447fa8cd1b3229c12f5
ulalawell <roman2000.usik@gmail.com>
2025-02-10 14:50:50 +0300

    feat: Add task hogwarts

--- hogwarts/courselist.go
+++ hogwarts/courselist.go
@@ -3,5 +3,51 @@
 package hogwarts
 
 func GetCourseList(prereqs map[string][]string) []string {
-	return []string{}
+	adjList := map[string][]string{}
+	inpDegree := map[string]int{}
+	visited := map[string]struct{}{}
+	courseList := []string{}
+
+	for course, prerequisites := range prereqs {
+		for _, prerequisite := range prerequisites {
+			adjList[prerequisite] = append(adjList[prerequisite], course)
+
+			if _, ok := inpDegree[prerequisite]; !ok {
+				inpDegree[prerequisite] = 0
+			}
+
+			inpDegree[course]++
+		}
+	}
+
+	st := []string{}
+	for course, degree := range inpDegree {
+		if degree == 0 {
+			st = append(st, course)
+			visited[course] = struct{}{}
+		}
+	}
+
+	for len(st) > 0 {
+		fromCourse := st[len(st)-1]
+		st = st[:len(st)-1]
+
+		if inpDegree[fromCourse] > 0 {
+			continue
+		}
+
+		visited[fromCourse] = struct{}{}
+		courseList = append(courseList, fromCourse)
+
+		for _, toCourse := range adjList[fromCourse] {
+			inpDegree[toCourse]--
+			st = append(st, toCourse)
+		}
+	}
+
+	if len(visited) < len(inpDegree) {
+		panic("Cycle found!")
+	}
+
+	return courseList
 }
