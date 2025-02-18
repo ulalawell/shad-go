fae6a3fc8f02b0682420ddea08225ea10a4e638a
ulalawell <roman2000.usik@gmail.com>
2025-02-18 23:16:43 +0300

    feat: Add otp task

--- otp/cipher.go
+++ otp/cipher.go
@@ -6,10 +6,51 @@ import (
 	"io"
 )
 
+type StreamCipherReader struct {
+	inputStream io.Reader
+	prng        io.Reader
+}
+
+type StreamCipherWriter struct {
+	outputStream io.Writer
+	prng         io.Reader
+}
+
 func NewReader(r io.Reader, prng io.Reader) io.Reader {
-	panic("implement me")
+	return &StreamCipherReader{
+		inputStream: r,
+		prng:        prng,
+	}
 }
 
 func NewWriter(w io.Writer, prng io.Reader) io.Writer {
-	panic("implement me")
+	return &StreamCipherWriter{
+		outputStream: w,
+		prng:         prng,
+	}
+}
+
+func (s *StreamCipherReader) Read(p []byte) (int, error) {
+	buf := make([]byte, len(p))
+
+	n, err := s.inputStream.Read(p)
+	if n > 0 {
+		_, _ = s.prng.Read(buf[:n]) // prng никогда не возвращает ошибку
+		for i := 0; i < n; i++ {
+			p[i] ^= buf[i]
+		}
+	}
+
+	return n, err
+}
+
+func (s *StreamCipherWriter) Write(p []byte) (int, error) {
+	buf := make([]byte, len(p))
+	_, _ = s.prng.Read(buf) // prng никогда не возвращает ошибку
+
+	for i := range p {
+		buf[i] ^= p[i]
+	}
+
+	return s.outputStream.Write(buf)
 }
