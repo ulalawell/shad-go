0653008444aee8e455b8da4a0a385b1efe75ec28
ulalawell <roman2000.usik@gmail.com>
2025-02-24 22:16:32 +0300

    feat: Add externalsort task

--- externalsort/io.go
+++ externalsort/io.go
@@ -2,10 +2,80 @@
 
 package externalsort
 
+import "io"
+
 type LineReader interface {
 	ReadLine() (string, error)
 }
 
 type LineWriter interface {
 	Write(l string) error
+}
+
+type LineReaderTool struct {
+	reader   io.Reader
+	diffData []byte
+}
+
+type LineWriterTool struct {
+	writer   io.Writer
+	diffData []byte
+}
+
+func NewReader(r io.Reader) LineReader {
+	return &LineReaderTool{
+		reader:   r,
+		diffData: []byte{},
+	}
+}
+
+func NewWriter(w io.Writer) LineWriter {
+	return &LineWriterTool{
+		writer:   w,
+		diffData: []byte{},
+	}
+}
+
+func (lr *LineReaderTool) ReadLine() (string, error) {
+	for {
+		if len(lr.diffData) != 0 {
+			for i := 0; i < len(lr.diffData); i++ {
+				if lr.diffData[i] == '\n' {
+					oneLine := string(lr.diffData[:i])
+					lr.diffData = lr.diffData[i+1:]
+					return oneLine, nil
+				}
+			}
+		}
+
+		p := make([]byte, 10)
+		n, err := lr.reader.Read(p)
+		p = p[:n]
+
+		for i := 0; i < len(p); i++ {
+			if p[i] == '\n' {
+				oneLine := string(lr.diffData) + string(p[:i])
+				lr.diffData = p[i+1:]
+				return oneLine, nil
+			}
+		}
+
+		if err != nil {
+			res := string(lr.diffData) + string(p)
+			lr.diffData = []byte{}
+			return res, err
+		}
+
+		lr.diffData = append(lr.diffData, p...)
+	}
+
+}
+
+func (lw *LineWriterTool) Write(l string) error {
+	l += "\n"
+	p := []byte(l)
+
+	_, err := lw.writer.Write(p)
+
+	return err
 }
--- externalsort/sort.go
+++ externalsort/sort.go
@@ -3,21 +3,78 @@
 package externalsort
 
 import (
+	"bufio"
+	"container/heap"
 	"io"
+	"os"
 )
 
-func NewReader(r io.Reader) LineReader {
-	panic("implement me")
+type StringHeap []string
+
+func (h StringHeap) Len() int           { return len(h) }
+func (h StringHeap) Less(i, j int) bool { return h[i] < h[j] }
+func (h StringHeap) Swap(i, j int)      { h[i], h[j] = h[j], h[i] }
+
+func (h *StringHeap) Push(x any) {
+	// Push and Pop use pointer receivers because they modify the slice's length,
+	// not just its contents.
+	*h = append(*h, x.(string))
 }
 
-func NewWriter(w io.Writer) LineWriter {
-	panic("implement me")
+func (h *StringHeap) Pop() any {
+	old := *h
+	n := len(old)
+	x := old[n-1]
+	*h = old[0 : n-1]
+	return x
 }
 
 func Merge(w LineWriter, readers ...LineReader) error {
-	panic("implement me")
+	h := &StringHeap{}
+	heap.Init(h)
+
+	for i := 0; i < len(readers); i++ {
+		for {
+			line, err := readers[i].ReadLine()
+			if err == nil || line != "" {
+				heap.Push(h, line)
+			}
+
+			if err == io.EOF {
+				break
+			} else if err != nil {
+				return err
+			}
+		}
+	}
+
+	for len(*h) > 0 {
+		err := w.Write(heap.Pop(h).(string))
+		if err != nil {
+			return err
+		}
+	}
+
+	return nil
 }
 
 func Sort(w io.Writer, in ...string) error {
-	panic("implement me")
+	readers := []LineReader{}
+
+	for _, file := range in {
+		f, err := os.Open(file)
+
+		if err != nil {
+			return err
+		}
+
+		readers = append(readers, NewReader(bufio.NewReader(f)))
+		defer f.Close()
+	}
+
+	lw := NewWriter(w)
+
+	err := Merge(lw, readers...)
+
+	return err
 }
