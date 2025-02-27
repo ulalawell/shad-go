f9e7119bbd67aa27beea96378b942e865a3eff2e
ulalawell <roman2000.usik@gmail.com>
2025-02-27 21:46:04 +0300

    fix: External sort

--- externalsort/io.go
+++ externalsort/io.go
@@ -2,7 +2,11 @@
 
 package externalsort
 
-import "io"
+import (
+	"bufio"
+	"io"
+	"strings"
+)
 
 type LineReader interface {
 	ReadLine() (string, error)
@@ -12,70 +16,35 @@ type LineWriter interface {
 	Write(l string) error
 }
 
-type LineReaderTool struct {
-	reader   io.Reader
-	diffData []byte
-}
-
-type LineWriterTool struct {
-	writer   io.Writer
-	diffData []byte
+type lineReader struct {
+	buffReader *bufio.Reader
 }
 
 func NewReader(r io.Reader) LineReader {
-	return &LineReaderTool{
-		reader:   r,
-		diffData: []byte{},
+	return &lineReader{
+		buffReader: bufio.NewReader(r),
 	}
+}
+
+func (r *lineReader) ReadLine() (string, error) {
+	st, err := r.buffReader.ReadString('\n')
+	return strings.Trim(st, "\n"), err
+}
+
+type lineWriter struct {
+	buffWriter *bufio.Writer
 }
 
 func NewWriter(w io.Writer) LineWriter {
-	return &LineWriterTool{
-		writer:   w,
-		diffData: []byte{},
+	return &lineWriter{
+		buffWriter: bufio.NewWriter(w),
 	}
 }
 
-func (lr *LineReaderTool) ReadLine() (string, error) {
-	for {
-		if len(lr.diffData) != 0 {
-			for i := 0; i < len(lr.diffData); i++ {
-				if lr.diffData[i] == '\n' {
-					oneLine := string(lr.diffData[:i])
-					lr.diffData = lr.diffData[i+1:]
-					return oneLine, nil
-				}
-			}
-		}
-
-		p := make([]byte, 10)
-		n, err := lr.reader.Read(p)
-		p = p[:n]
-
-		for i := 0; i < len(p); i++ {
-			if p[i] == '\n' {
-				oneLine := string(lr.diffData) + string(p[:i])
-				lr.diffData = p[i+1:]
-				return oneLine, nil
-			}
-		}
-
-		if err != nil {
-			res := string(lr.diffData) + string(p)
-			lr.diffData = []byte{}
-			return res, err
-		}
-
-		lr.diffData = append(lr.diffData, p...)
+func (w *lineWriter) Write(l string) error {
+	_, err := w.buffWriter.WriteString(l + "\n")
+	if err != nil {
+		return err
 	}
-
-}
-
-func (lw *LineWriterTool) Write(l string) error {
-	l += "\n"
-	p := []byte(l)
-
-	_, err := lw.writer.Write(p)
-
-	return err
+	return w.buffWriter.Flush()
 }
--- externalsort/sort.go
+++ externalsort/sort.go
@@ -3,78 +3,146 @@
 package externalsort
 
 import (
-	"bufio"
 	"container/heap"
+	"errors"
 	"io"
 	"os"
 )
 
-type StringHeap []string
-
-func (h StringHeap) Len() int           { return len(h) }
-func (h StringHeap) Less(i, j int) bool { return h[i] < h[j] }
-func (h StringHeap) Swap(i, j int)      { h[i], h[j] = h[j], h[i] }
-
-func (h *StringHeap) Push(x any) {
-	// Push and Pop use pointer receivers because they modify the slice's length,
-	// not just its contents.
-	*h = append(*h, x.(string))
+// mergeItem хранит строку и источник.
+type mergeItem struct {
+	value  string
+	source int
 }
 
-func (h *StringHeap) Pop() any {
-	old := *h
-	n := len(old)
-	x := old[n-1]
-	*h = old[0 : n-1]
-	return x
+// priorityQueue реализует heap.Interface для сортировки строк.
+type priorityQueue []mergeItem
+
+func (pq priorityQueue) Len() int            { return len(pq) }
+func (pq priorityQueue) Less(i, j int) bool  { return pq[i].value < pq[j].value }
+func (pq priorityQueue) Swap(i, j int)       { pq[i], pq[j] = pq[j], pq[i] }
+func (pq *priorityQueue) Push(x interface{}) { *pq = append(*pq, x.(mergeItem)) }
+func (pq *priorityQueue) Pop() interface{} {
+	n := len(*pq)
+	item := (*pq)[n-1]
+	*pq = (*pq)[:n-1]
+	return item
 }
 
+// Merge сливает отсортированные входные данные.
 func Merge(w LineWriter, readers ...LineReader) error {
-	h := &StringHeap{}
-	heap.Init(h)
+	pq := &priorityQueue{}
+	heap.Init(pq)
 
-	for i := 0; i < len(readers); i++ {
-		for {
-			line, err := readers[i].ReadLine()
-			if err == nil || line != "" {
-				heap.Push(h, line)
+	for i, r := range readers {
+		l, err := r.ReadLine()
+		if err != nil {
+			if errors.Is(err, io.EOF) {
+				if l != "" {
+					heap.Push(pq, mergeItem{value: l, source: -1})
+				}
+				continue
 			}
-
-			if err == io.EOF {
-				break
-			} else if err != nil {
-				return err
-			}
+			return err
 		}
+		heap.Push(pq, mergeItem{value: l, source: i})
 	}
 
-	for len(*h) > 0 {
-		err := w.Write(heap.Pop(h).(string))
+	for pq.Len() > 0 {
+		item := heap.Pop(pq).(mergeItem)
+		if err := w.Write(item.value); err != nil {
+			return err
+		}
+		if item.source == -1 {
+			continue
+		}
+		l, err := readers[item.source].ReadLine()
+		if err != nil {
+			if errors.Is(err, io.EOF) {
+				if l != "" {
+					heap.Push(pq, mergeItem{value: l, source: -1})
+				}
+				continue
+			}
+			return err
+		}
+		heap.Push(pq, mergeItem{value: l, source: item.source})
+	}
+	return nil
+}
+
+// Sort сортирует содержимое файлов и записывает результат.
+func Sort(w io.Writer, in ...string) error {
+	var readers []LineReader
+
+	for _, file := range in {
+		if err := sortFile(file); err != nil {
+			return err
+		}
+
+		f, err := os.Open(file)
 		if err != nil {
 			return err
 		}
+		defer f.Close()
+
+		readers = append(readers, NewReader(f))
+	}
+
+	return Merge(NewWriter(w), readers...)
+}
+
+func sortFile(file string) error {
+	pq := &priorityQueue{}
+	heap.Init(pq)
+
+	if err := readAndPushLines(file, pq); err != nil {
+		return err
+	}
+
+	return writeSortedLines(file, pq)
+}
+
+func readAndPushLines(file string, pq *priorityQueue) error {
+	f, err := os.Open(file)
+	if err != nil {
+		return err
+	}
+	defer f.Close()
+
+	r := NewReader(f)
+	for {
+		l, err := r.ReadLine()
+		if err != nil {
+			if errors.Is(err, io.EOF) {
+				if l != "" {
+					heap.Push(pq, mergeItem{value: l, source: 0})
+				}
+				break
+			}
+
+			return err
+		}
+		heap.Push(pq, mergeItem{value: l, source: 0})
 	}
 
 	return nil
 }
 
-func Sort(w io.Writer, in ...string) error {
-	readers := []LineReader{}
-
-	for _, file := range in {
-		f, err := os.Open(file)
-
-		if err != nil {
-			return err
-		}
-
-		readers = append(readers, NewReader(bufio.NewReader(f)))
-		defer f.Close()
+func writeSortedLines(file string, pq *priorityQueue) error {
+	f, err := os.Create(file)
+	defer f.Close()
+	if err != nil {
+		return err
 	}
 
-	lw := NewWriter(w)
+	writer := NewWriter(f)
+	for pq.Len() > 0 {
+		item := heap.Pop(pq).(mergeItem)
+		if err = writer.Write(item.value); err != nil {
+			return err
+		}
+	}
 
-	err := Merge(lw, readers...)
-
-	return err
+	return nil
 }
