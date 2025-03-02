af584b90947739063723f2a18e244bee2f04c238
ulalawell <roman2000.usik@gmail.com>
2025-03-02 16:58:47 +0300

    feat: Add ciletters task

--- ciletters/letter.go
+++ ciletters/letter.go
@@ -2,6 +2,45 @@
 
 package ciletters
 
+import (
+	"bytes"
+	_ "embed"
+	"strings"
+	"text/template"
+)
+
+//go:embed notification.tmpl
+var tmplFile string // Встраиваем файл как строку
+
+var countLinesGitlabRunner = 10
+var countBytesHashCommit = 8
+var indent = "\n            "
+
 func MakeLetter(n *Notification) (string, error) {
-	return "", nil
+	funcMap := template.FuncMap{
+		"shortenHashCommit": func(s string) string {
+			return s[:countBytesHashCommit]
+		},
+		"shortenLog": func(s string) string {
+			lines := strings.Split(s, "\n")
+
+			if len(lines) > countLinesGitlabRunner {
+				return strings.Join(lines[len(lines)-countLinesGitlabRunner:], indent)
+			}
+			return strings.Join(lines, indent)
+		},
+	}
+
+	var buf bytes.Buffer
+
+	tmpl, err := template.New("notification").Funcs(funcMap).Parse(tmplFile)
+	if err != nil {
+		return "", err
+	}
+	err = tmpl.Execute(&buf, n)
+	if err != nil {
+		return "", err
+	}
+
+	return buf.String(), nil
 }
