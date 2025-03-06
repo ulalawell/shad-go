0909e5705428f4dced5abe504de4719571671045
ulalawell <roman2000.usik@gmail.com>
2025-03-06 22:51:04 +0300

    feat: Add fetchall task

--- fetchall/main.go
+++ fetchall/main.go
@@ -2,6 +2,37 @@
 
 package main
 
-func main() {
+import (
+	"fmt"
+	"net/http"
+	"os"
+	"sync"
+	"time"
+)
 
+func main() {
+	urls := os.Args[1:]
+	wg := sync.WaitGroup{}
+
+	currentTime := time.Now()
+	for _, url := range urls {
+		wg.Add(1)
+		go func(url string) {
+			defer wg.Done()
+			resp, err := http.Get(url)
+
+			if err != nil {
+				fmt.Println(err)
+				return
+			}
+
+			defer resp.Body.Close()
+			deltaTime := time.Since(currentTime)
+			fmt.Printf("url: %s, time: %s, ContentLength: %d\n", url, deltaTime.String(), resp.ContentLength)
+
+		}(url)
+	}
+
+	wg.Wait()
+	fmt.Printf("total time: %s", time.Since(currentTime).String())
 }
