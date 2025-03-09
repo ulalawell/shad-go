3ba92e1df164c104887f439f865a978d33d8a660
ulalawell <roman2000.usik@gmail.com>
2025-03-09 13:15:48 +0300

    feat: Add once task

--- once/once.go
+++ once/once.go
@@ -2,30 +2,24 @@
 
 package once
 
-// Once describes an object that will perform exactly one action.
 type Once struct {
+	ch   chan struct{}
+	done chan struct{}
 }
 
-// New creates Once.
 func New() *Once {
-	return nil
+	return &Once{
+		ch:   make(chan struct{}, 1),
+		done: make(chan struct{}),
+	}
 }
 
-// Do calls the function f if and only if Do is being called for the
-// first time for this instance of Once. In other words, given
-// 	once := New()
-// if once.Do(f) is called multiple times, only the first call will invoke f,
-// even if f has a different value in each invocation. A new instance of
-// Once is required for each function to execute.
-//
-// Do is intended for initialization that must be run exactly once.
-//
-// Because no call to Do returns until the one call to f returns, if f causes
-// Do to be called, it will deadlock.
-//
-// If f panics, Do considers it to have returned; future calls of Do return
-// without calling f.
-//
 func (o *Once) Do(f func()) {
-
+	select {
+	case o.ch <- struct{}{}:
+		defer close(o.done)
+		f()
+	default:
+		<-o.done
+	}
 }
